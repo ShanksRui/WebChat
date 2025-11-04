bdc2ba4e4bb87e4ed5aef7122efd0f8af2460717
Luiz Felipe Vianna Vieira <luiz.vianna@MDIC.COLABORAGOV.GOV.BR>
2025-11-04 16:34:42 -0300

    entities.chat criado

---
+++ src/main/java/com/development/webchat/model/entities/Chat.java
@@ -0,0 +1,67 @@
+package com.development.webchat.model.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Chat implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private List<User> users = new ArrayList<>();
+	private Instant lastActivity;
+	private Instant firstChat;
+	
+	
+	public Chat() {
+		
+	}
+	
+	public Chat(String id, Instant lastActivity, Instant firstChat) {
+		this.id = id;
+		this.lastActivity = lastActivity;
+		this.firstChat = firstChat;
+	}
+	public String getId() {
+		return id;
+	}
+	public List<User> getUsers() {
+		return users;
+	}
+	public Instant getLastActivity() {
+		return lastActivity;
+	}
+	public Instant getFirstChat() {
+		return firstChat;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+	public void setLastActivity(Instant lastActivity) {
+		this.lastActivity = lastActivity;
+	}
+	public void setFirstChat(Instant firstChat) {
+		this.firstChat = firstChat;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Chat other = (Chat) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
